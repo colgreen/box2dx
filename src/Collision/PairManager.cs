82612485660b7a434f4a3c5067816035e6f59e95
ihar3d <ihar3d@12e8ec02-c647-0410-85fd-b139666dd809>
2008-03-09 21:09:51 +0000

    Copyright (c) 2008 Ihar Kalasouski http://code.google.com/p/box2dx

--- src/Collision/PairManager.cs
+++ src/Collision/PairManager.cs
@@ -1,4 +1,25 @@
-﻿// The pair manager is used by the broad-phase to quickly add/remove/find pairs
+﻿/*
+  Box2DX Copyright (c) 2008 Ihar Kalasouski http://code.google.com/p/box2dx
+  Box2D original C++ version Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+// The pair manager is used by the broad-phase to quickly add/remove/find pairs
 // of overlapping proxies. It is based closely on code provided by Pierre Terdiman.
 // http://www.codercorner.com/IncrementalSAP.txt
 
