5c0aaf248252ee49062b0a8731e6e9c91e3b8475
ihar3d <ihar3d@12e8ec02-c647-0410-85fd-b139666dd809>
2008-03-18 16:04:02 +0000

    PolyAndCircleContact was ported.

--- src/Dynamics/Contacts/PolyAndCircleContact.cs
+++ src/Dynamics/Contacts/PolyAndCircleContact.cs
@@ -23,9 +23,89 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using Box2DX.Collision;
+using Box2DX.Common;
+
 namespace Box2DX.Dynamics.Contacts
 {
-	public class PolyAndCircleContact
+	public class PolyAndCircleContact : Contact
 	{
+		public Manifold _manifold = new Manifold();
+
+		public override Manifold GetManifolds()
+		{
+			return _manifold;
+		}
+
+		public PolyAndCircleContact(Shape s1, Shape s2)
+			: base(s1, s2)
+		{
+			Box2DXDebug.Assert(_shape1._type == ShapeType.PolygonShape);
+			Box2DXDebug.Assert(_shape2._type == ShapeType.CircleShape);
+			_manifold.PointCount = 0;
+			_manifold.Points[0].NormalForce = 0.0f;
+			_manifold.Points[0].TangentForce = 0.0f;
+		}
+
+		public override void Evaluate(ContactListener listener)
+		{
+			Body b1 = _shape1.GetBody();
+			Body b2 = _shape2.GetBody();
+
+			//memcpy(&m0, &m_manifold, sizeof(b2Manifold));
+			Manifold m0 = new Manifold();
+			m0.Normal = _manifold.Normal;
+			m0.PointCount = _manifold.PointCount;
+			m0.Points = _manifold.Points;
+
+			Collision.Collision.CollidePolygonAndCircle(ref _manifold, (PolygonShape)_shape1, b1._xf, (CircleShape)_shape2, b2._xf);
+
+			if (_manifold.PointCount > 0)
+			{
+				_manifoldCount = 1;
+				if (m0.PointCount == 0)
+				{
+					_manifold.Points[0].ID.Features.Flip |= Collision.Collision.NewPoint;
+				}
+				else
+				{
+					_manifold.Points[0].ID.Features.Flip &= ~Collision.Collision.NewPoint;
+				}
+			}
+			else
+			{
+				_manifoldCount = 0;
+				if (m0.PointCount > 0 && listener != null)
+				{
+					ContactPoint cp = new ContactPoint();
+					cp.Shape1 = _shape1;
+					cp.Shape2 = _shape2;
+					cp.Normal = m0.Normal;
+					cp.Position = Common.Math.Mul(b1._xf, m0.Points[0].LocalPoint1);
+					cp.Separation = m0.Points[0].Separation;
+					cp.NormalForce = m0.Points[0].NormalForce;
+					cp.TangentForce = m0.Points[0].TangentForce;
+					cp.ID = m0.Points[0].ID;
+					listener.Remove(cp);
+				}
+			}
+		}
+
+		public static Contact Create(Shape shape1, Shape shape2)
+		{
+			return new PolyAndCircleContact(shape1, shape2);
+		}
+
+		public static void Destroy(Contact contact)
+		{
+			Box2DXDebug.Assert(contact._shape1._body._contactList != contact._node1);
+			Box2DXDebug.Assert(contact._shape1._body._contactList != contact._node2);
+			Box2DXDebug.Assert(contact._shape2._body._contactList != contact._node1);
+			Box2DXDebug.Assert(contact._shape2._body._contactList != contact._node2);
+
+			if (contact is IDisposable)
+				(contact as IDisposable).Dispose();
+			contact = null;
+		}
 	}
 }
