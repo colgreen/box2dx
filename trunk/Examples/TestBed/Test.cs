68b16d168712b4921af3ef16e63743b6fb4cd4c4
ihar3d <ihar3d@12e8ec02-c647-0410-85fd-b139666dd809>
2008-04-15 11:23:01 +0000

    1) All features of Box2D were ported. 2) A few critical issues in broadphase collision detection were fixed, but still some exist. WIP 3) A few tests were added to TestBed.

--- trunk/Examples/TestBed/Test.cs
+++ trunk/Examples/TestBed/Test.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+  Box2DX Copyright (c) 2008 Ihar Kalasouski http://code.google.com/p/box2dx
+  Box2D original C++ version Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -188,7 +209,12 @@ namespace TestBed
 		public static TestEntry[] g_testEntries = new TestEntry[]
 		{
 			new TestEntry("Simple Test", SimpleTest.Create),
-			//new TestEntry("Pyramid", Pyramid.Create)			
+			new TestEntry("Vertical Stack", VerticalStack.Create),
+			new TestEntry("Pyramid", Pyramid.Create),
+			new TestEntry("Bridge", Bridge.Create),
+			new TestEntry("Pulleys", Pulleys.Create),
+			new TestEntry("Gears", Gears.Create),
+			new TestEntry("Broad Phase", BroadPhaseTest.Create)
 		};
 		public static int k_maxContactPoints = 2048;
 
@@ -229,9 +255,17 @@ namespace TestBed
 
 		public void Dispose()
 		{
-			// By deleting the world, we delete the bomb, mouse joint, etc.
-			_world.Dispose();
-			_world = null;
+			Dispose(true);
+		}
+
+		protected virtual void Dispose(bool state)
+		{
+			if (state)
+			{
+				// By deleting the world, we delete the bomb, mouse joint, etc.
+				_world.Dispose();
+				_world = null;
+			}
 		}
 
 		public void SetTextLine(int line) { _textLine = line; }
@@ -331,7 +365,7 @@ namespace TestBed
 			_bomb.SetMassFromShapes();
 		}
 
-		public void Step(Settings settings)
+		public virtual void Step(Settings settings)
 		{
 			float timeStep = settings.hz > 0.0f ? 1.0f / settings.hz : 0.0f;
 
